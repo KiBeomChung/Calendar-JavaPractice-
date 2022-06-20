4c4258fa4351c6216dc334a7afc08269954703d7
kibeomChung <kibeom5118@naver.com>
2022-06-20 22:57:29 +0900

    반복횟수를 입력받고 그 횟수만큼 반복하기

--- src/calendar/Calendar.java
+++ src/calendar/Calendar.java
@@ -3,9 +3,15 @@ package calendar;
 import java.util.Scanner;
 
 public class Calendar {
-	
+
+	private static final int[] MAX_DAYS = { 31, 28, 31, 30, 31, 30, 31, 30, 31, 30, 31, 30 };
+
+	public int maxDateOfMonthByArray(int month) {
+		return MAX_DAYS[month - 1];
+	} // 배열을 이용한 달력의 마지막 날 구하기!
+
 	public int maxDateOfMonth(int month) {
-		
+
 		int maxDate = 0;
 
 		if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) {
@@ -20,8 +26,8 @@ public class Calendar {
 			maxDate = 30;
 		}
 		return maxDate;
-	}
-	
+	} // 조건문을 이용한 마지막 날 구하기!
+
 	public void printMonthDate() {
 		System.out.println("일 월 화 수 목 금 토");
 		System.out.println("================");
@@ -35,17 +41,25 @@ public class Calendar {
 		// TODO Auto-generated method stub
 
 		Scanner scanner = new Scanner(System.in);
+		System.out.println("반복할 횟수를 입력하세요 : ");
+
+		int repetition = scanner.nextInt();
 		Calendar cal = new Calendar();
-		System.out.println("달을 입력세요: ");
+
+		for (int i = 0; i < repetition; i++) {
+
+			System.out.println("달을 입력세요: ");
+			int month = scanner.nextInt();
+
+			System.out.printf("%d의 마지막 일은 %d 입니다.\n", month, cal.maxDateOfMonth(month));
+		}
 		
-		int month = scanner.nextInt();
-		
-		System.out.printf("%d의 마지막 일은 %d 입니다.", month, cal.maxDateOfMonth(month));
-		
-		cal.printMonthDate();
-		
+		System.out.println("==================================================================");
+
+		//cal.printMonthDate();
+
 		scanner.close();
-		
+
 	}
 
 }
