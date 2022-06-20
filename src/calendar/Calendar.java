d86c7fbfa911527c7853498e28bff29298000a57
kibeomChung <kibeom5118@naver.com>
2022-06-20 22:28:12 +0900

    각 달의 최대 일 수 구하기 (함수 이용, 조건문, 배)

--- src/calendar/Calendar.java
+++ src/calendar/Calendar.java
@@ -3,42 +3,48 @@ package calendar;
 import java.util.Scanner;
 
 public class Calendar {
+	
+	public int maxDateOfMonth(int month) {
+		
+		int maxDate = 0;
+
+		if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) {
+			maxDate = 31;
+		}
+
+		else if (month == 2) {
+			maxDate = 28;
+		}
+
+		else if (month == 4 || month == 6 || month == 9 || month == 11) {
+			maxDate = 30;
+		}
+		return maxDate;
+	}
+	
+	public void printMonthDate() {
+		System.out.println("일 월 화 수 목 금 토");
+		System.out.println("================");
+		System.out.println("1 2 3 4 5 6 7 ");
+		System.out.println("8 9 10 11 12 13 14");
+		System.out.println("15 16 17 18 19 20 21");
+		System.out.println("22 23 24 25 26 27 28");
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("일 월 화 수 목 금 토");
-//		System.out.println("================");
-//		System.out.println("1 2 3 4 5 6 7 ");
-//		System.out.println("8 9 10 11 12 13 14");
-//		System.out.println("15 16 17 18 19 20 21");
-//		System.out.println("22 23 24 25 26 27 28");
-	
-//		int n = 1;
-//		
-//		for(int j = 0;j < 4; j++)
-//		{
-//			for(int i = 0;i < 7; i++)
-//			{
-//				System.out.printf("%2d", n++);
-//			}
-//			System.out.println("");
-//		}
 
-	Scanner scanner = new Scanner(System.in);
-	System.out.println("달을 입력세요: ");
-	
-	int month = scanner.nextInt();
-	
-	if(month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12)
-	{
-		System.out.println("31");
-	}else if(month == 2)
-	{
-		System.out.println("28");	
-	}else if(month == 4 || month == 6 || month == 9 || month ==11)
-	{
-		System.out.println("30");
-	}
+		Scanner scanner = new Scanner(System.in);
+		Calendar cal = new Calendar();
+		System.out.println("달을 입력세요: ");
+		
+		int month = scanner.nextInt();
+		
+		System.out.printf("%d의 마지막 일은 %d 입니다.", month, cal.maxDateOfMonth(month));
+		
+		cal.printMonthDate();
+		
+		scanner.close();
 		
 	}
 
